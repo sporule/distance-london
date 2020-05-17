80634b23008dcefb8d610d41aaa62dfdafed07d2
jie <jerrielu@gmail.com>
2020-05-17 21:19:18 +0000

    udpated allow_origin

--- api/config/liv.py
+++ api/config/liv.py
@@ -3,6 +3,6 @@ import datetime
 # Secret key for hashing
 SECRET_KEY = 'AT@u@5HgZ/g0zK)a<-1l1[%xihmk-|'
 # database setting
-ALLOW_ORIGIN = ['https://distance.london']
+ALLOW_ORIGIN = 'https://distance.london'
 MONGO_URI=""
 PIN=""
