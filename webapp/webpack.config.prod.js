de0aec4d4a0fbc69292e13f82a8c8b9a4de10c1a
jie <jerrielu@gmail.com>
2020-05-24 20:26:43 +0000

    optimised pwa

--- webapp/webpack.config.prod.js
+++ webapp/webpack.config.prod.js
@@ -104,7 +104,7 @@ module.exports={
         ,
         {
           src: path.resolve('src/publish_assets/logo-ios.png'),
-          sizes: 1024, // multiple sizes
+          sizes: [512,1024], // multiple sizes
           ios: 'startup'
         }
       ],
