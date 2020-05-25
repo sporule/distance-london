2d487dad25e697009b2242e530ff712c41ccc7ef
jie <jerrielu@gmail.com>
2020-05-25 13:12:36 +0000

    fixed some wording in the map page

--- webapp/src/pages/MapPage.js
+++ webapp/src/pages/MapPage.js
@@ -65,10 +65,9 @@ class MapPage extends React.Component {
                   <Popup>
                     <div>
                       <p>Pedestrian Count: {camera.count}</p>
-                      <p>The number above was measured at {new Date(camera.update_time).toTimeString().slice(0, 8)}, please use live TFL Camera below to see the most recent situation:</p>
+                      <p>The number above was measured at {new Date(camera.update_time).toTimeString().slice(0, 8)}</p>
+                      <p>please use live TFL Camera below to see the most recent situation:</p>
                       {liveCamera}
-                      }
-
                     </div>
                   </Popup>
                 </Circle>
