e65bbac3f05e6815653fd6ab57ff7627fab7e980
jie <jerrielu@gmail.com>
2020-05-25 11:53:34 +0000

    updated camera logic

--- webapp/src/pages/MapPage.js
+++ webapp/src/pages/MapPage.js
@@ -65,7 +65,7 @@ class MapPage extends React.Component {
                   <Popup>
                     <div>
                       <p>Pedestrian Count: {camera.count}</p>
-                      <p>The number above was measured at {new Date(camera.update_time).toTimeString().slice(0, 8)}, please use TFS Live Camera below to get the most up to date situation:</p>
+                      <p>The number above was measured at {new Date(camera.update_time).toTimeString().slice(0, 8)}, please use TFS Live Camera below to get the most recent situation:</p>
                       {liveCamera}
                       }
 
