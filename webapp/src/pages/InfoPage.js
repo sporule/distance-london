bdaa6753858be4b50409be8f9b2b6b79150e3c10
jie <jerrielu@gmail.com>
2020-05-28 21:53:44 +0000

    removed donation info

--- webapp/src/pages/InfoPage.js
+++ webapp/src/pages/InfoPage.js
@@ -16,9 +16,7 @@ class InfoPage extends React.Component {
     return (
       <Ons.Page>
         <Ons.Card>
-          <p>As a starter data engineer, I wanted to help to build a safer London during this special time.</p>
           <p>Distance.London is my first project using AI detection technology to show users whether their destinations are crowded, in order to help users to keep social distance and stay safe.</p>
-          <p>Join us to Support NHS with the Social Distance AI, Distance.London at <a target="_blank" href="https://www.justgiving.com/fundraising/distance-london">Just Giving Fundraising</a>.</p>
         </Ons.Card>
       </Ons.Page>
     );
